848c6db47b6773240303318ccdd34c3fb3ff415b
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-27 17:54:02 +0500

    Add project files.

---
+++ Tyuiu.KovalenkoMA.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KovalenkoMA.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
