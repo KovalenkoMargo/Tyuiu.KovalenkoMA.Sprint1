2a0a67c6c0f4fc52fed64449aed6a7cb639a39a8
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-28 22:21:49 +0500

    Task2

---
+++ Tyuiu.KovalenkoMA.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KovalenkoMA.Sprint1.Task2.V29.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Tyuiu.KovalenkoMA.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3600;
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(60, res);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KovalenkoMA.Sprint1.Task2.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Коваленко М. А. | ПИНб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("*вычисляет результат по формуле и печатает результат на экране.           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ConvertSecondsToHours(x));
+
+        Console.ReadLine();
+    }
+}
