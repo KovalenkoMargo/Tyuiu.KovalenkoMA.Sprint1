ed82db61467041ab36503ca2dce842132b8ac2f4
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-29 14:50:13 +0500

    Task5

---
+++ Tyuiu.KovalenkoMA.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KovalenkoMA.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int k = 1;
+            var res = ds.Calculate(k);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
