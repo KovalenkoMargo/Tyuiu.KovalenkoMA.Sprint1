1f24004528e3454fa0f3923943f8e55ece3a0245
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-30 20:56:49 +0500

    Task7

---
+++ Tyuiu.KovalenkoMA.Sprint1.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KovalenkoMA.Sprint1.Task7.V10.Lib
+{
+    public class DataService : ISprint1Task7V10
+    {
+        public double Calculate(double x)
+        {
+            double r1 = 2 * (1 / Math.Tan(3 * x));
+            double r2 = Math.Log(Math.Cos(x));
+            double r3 = Math.Log(1 + Math.Pow(x, 2));
+            return Math.Round(r1 - r2 / r3, 3);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KovalenkoMA.Sprint1.Task7.V10.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(-13.142, res);
+        }
+    }
+}
